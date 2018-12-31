4de3981456b6df7c61697f84f8809ea68cc59e0a
andrejmalykhin <Andrej.Malykhin.13@gmail.com>
2018-12-31 18:38:53 +0200

    20. Hibernate Configuration with Annotations

---
+++ src/com/giggs13/springdemo/hibernate/entity/Student.java
@@ -0,0 +1,30 @@
+package com.giggs13.springdemo.hibernate.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+@Data
+public class Student {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
